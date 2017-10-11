674d46aa99fca0d6da27fe9e333af62acdb24b5e
bradfordwagner <wagner.bradford@gmail.com>
2017-10-10 20:28:57 -0400

    Added redux / material

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {applyMiddleware, compose, createStore} from 'redux'
+import reducer from './redux/reducers'
+import {Provider} from 'react-redux';
+import {MuiThemeProvider} from "material-ui";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    const result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd(action.type)
+    return result
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+    reducer,
+    composeEnhancers(
+        applyMiddleware(logger)
+    )
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <App/>
+        </MuiThemeProvider>
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
