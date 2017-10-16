6187324845459a559c19ee68fb1051d386113ccf
bwagner <bradford.wagner@blackrock.com>
2017-10-15 21:01:32 -0400

    Added icons

--- frontend/src/components/Sorter.js
+++ frontend/src/components/Sorter.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react'
-import {Card, CardHeader, CardTitle} from "material-ui";
+import {Card, CardHeader, MenuItem, SelectField} from "material-ui";
 
 export class Sorter extends Component {
+    state = {}
+
+    componentDidMount() {
+        console.info("mounted")
+        this.setState((state, props) => {
+            return {
+                selectedValue: props.options[0].name ? props.options[0].name : "nothing"
+            }
+        })
+    }
+
     render() {
         return (
             <Card>
                 <CardHeader title={this.props.title}/>
-                {this.props.options.map(option => (
-                    <p>{option.name}</p>
-                ))}
-                <p>sorter</p>
+
+                <div className="card-inset">
+                    <SelectField
+                        floatingLabelText="sort by"
+                        value={this.state.selectedValue}
+                        onChange={this.handleChange}
+                        autoWidth={true}
+                    >
+                        {this.props.options.map((option, index) => (
+                            <MenuItem value={option.name} primaryText={option.name}/>
+                        ))}
+                    </SelectField>
+                </div>
             </Card>
         )
     }
