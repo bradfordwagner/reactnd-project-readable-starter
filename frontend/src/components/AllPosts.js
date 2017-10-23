ef0beb69320729b8752a70e00ef3d44a1c535d30
bwagner <bradford.wagner@blackrock.com>
2017-10-23 11:31:44 -0400

    Optimized imports

--- frontend/src/components/AllPosts.js
+++ frontend/src/components/AllPosts.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react'
 import actions from '../redux/actions'
 import {connect} from "react-redux";
-import Post from './Post'
-import {DESCENDING, Sorter, SorterOption} from "./Sorter";
 import "./AllPosts.css"
-import {Dialog, RaisedButton} from "material-ui";
-import EditPost from "./EditPost";
 import {withRouter} from "react-router-dom";
 import PostsWithSorter from "./PostsWithSorter";
 
